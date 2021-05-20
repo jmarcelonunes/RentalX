8442915b9371469e9a8fa0a0be53572b6f6a747b
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-05-20 13:41:10 -0300

    Adds users authentications; jwt; custom error handling.

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest{
+    email: string;
+    password: string;
+}
+
+interface IReturn{
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password } : IRequest) : Promise<IReturn> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Email/Password incorrect');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new AppError('Email/Password incorrect');
+    }
+
+    const token = sign({}, '81afdf1091a3ea35298d77ccb1fef0cf', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IReturn = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
