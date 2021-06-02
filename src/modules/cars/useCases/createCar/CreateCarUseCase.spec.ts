1fd057256b22d74439e31f1d51cf498602a6135e
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-06-02 11:47:23 -0300

    Create cars use case; Admin seed

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+  it('Should be able to create a new car', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Classe C',
+      description: 'Mercedes classe C prata',
+      daily_rate: 300,
+      license_plate: 'PAW-7878',
+      fine_amount: 60,
+      brand: 'Mercedes',
+      category_id: 'category',
+    });
+    expect(car).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a car with the same license plate',
+    async () => {
+      expect(async () => {
+        await createCarUseCase.execute({
+          name: 'Classe C',
+          description: 'Mercedes classe C prata',
+          daily_rate: 300,
+          license_plate: 'PAW-7878',
+          fine_amount: 60,
+          brand: 'Mercedes',
+          category_id: 'category',
+        });
+
+        await createCarUseCase.execute({
+          name: 'Classe A',
+          description: 'Mercedes classe A prata',
+          daily_rate: 300,
+          license_plate: 'PAW-7878',
+          fine_amount: 60,
+          brand: 'Mercedes',
+          category_id: 'category',
+        });
+      }).rejects.toBeInstanceOf(AppError);
+    });
+
+  it('Should not be able to create an available car by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Classe C',
+      description: 'Mercedes classe C prata',
+      daily_rate: 300,
+      license_plate: 'PAQ-7878',
+      fine_amount: 60,
+      brand: 'Mercedes',
+      category_id: 'category',
+    });
+
+    expect(car.available).toBeTruthy();
+  });
+});
