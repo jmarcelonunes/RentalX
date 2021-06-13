aeb1404b5c26f256d776b20abeef6775babd6a93
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-06-13 16:48:15 -0300

    Implements list rentals by user

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUser = new ListRentalsByUserController();
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
 
 rentalRoutes.post('/devolution/:id', ensureAuthenticated, devolutionRentalController.handle);
+
+rentalRoutes.get('/user', ensureAuthenticated, listRentalsByUser.handle);
 
 export { rentalRoutes };
