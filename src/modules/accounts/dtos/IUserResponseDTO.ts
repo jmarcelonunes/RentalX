cbf71e35d8dfa793ead197b4b6f736292714ddae
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-06-27 23:21:42 -0300

    Adds user profile route

---
+++ src/modules/accounts/dtos/IUserResponseDTO.ts
@@ -0,0 +1,9 @@
+interface IUsersResponseDTO {
+    email: string;
+    name: string;
+    id: string;
+    avatar: string;
+    driver_license: string;
+    avatar_url();
+}
+export { IUsersResponseDTO };
