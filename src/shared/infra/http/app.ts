6d809c2d4328e6e9b0c33a8aa5c9fad8eb466bf0
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-07-10 14:58:36 -0300

    Implements rate limiter with redis

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import swaggerUi from 'swagger-ui-express';
 import 'dotenv/config';
 import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
@@ -14,6 +15,8 @@ import '@shared/container';
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
